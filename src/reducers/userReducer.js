eccecb30199c1811f15063793822ce59ef7fa2fa
carmelobalzano <balzanoitaliano@gmail.com>
2020-03-11 11:23:45 -0400

    purchase detail component

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -6,7 +6,17 @@ import PurchaseDetail from '../components/account/purchaseDetail';
 
 const INITIAL_STATE = {
     purchases: [],
-    purchaseDetail: {}
+    purchaseDetail: {
+        _id: -1,
+        total: 0,
+        orderNumber: '',
+        orderDate: null,
+        creditCard: '',
+        user: {
+            name: '',
+            shippingAddress: ''
+        }
+    }
 }
 
 export default function(state = INITIAL_STATE, action) {
