9f3b4d1fef6f9a994cb30a46368888e0a9b88f11
carmelobalzano <balzanoitaliano@gmail.com>
2020-03-11 12:16:56 -0400

    styled purchase detail

--- src/components/account/purchaseDetail.js
+++ src/components/account/purchaseDetail.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
@@ -11,33 +10,40 @@ function PurchaseDetailLabel({className, title, value}) {
     )
 }
 
+
+// total: 19.40,
+// creditCard: '-0000',
+
+
 class PurchaseDetail extends Component {
     render() {
         const { className, orderNumber, orderDate, user, total, creditCard } = this.props;
         const { name, shippingAddress } = user;
-
+        const nameAddress = `${name}       ${shippingAddress}`
         return (
             <div className={`${className} purchase-detail`}>
                 <PurchaseDetailLabel
-                    className='purchase-detail__order-number'
-                    title='Order Number'
-                    value={orderNumber}/>
+                 className='purchase-detail__order-number'
+                 title='Order Number'
+                 value={orderNumber}/>
                 <PurchaseDetailLabel
-                    className='purchase-detail__order-date'
-                    title='Order Date'
-                    value={orderDate}/>
-                 <PurchaseDetailLabel
-                    className='purchase-detail__shipping'
-                    title='Shipping Address'
-                    value={`${name}\n${shippingAddress}`}/>
+                 className='purchase-detail__order-date'
+                 title='Order Date'
+                 value={orderDate}/>
                 <PurchaseDetailLabel
-                    className='purchase-detail__total'
-                    title='Total'
-                    value={total}/>
+                 className='purchase-detail__shipping-address'
+                 title='Shipping Address'
+                 value={nameAddress}/>
                 <PurchaseDetailLabel
-                    className='purchase-detail__creditCard'
-                    title='Credit Card'
-                    value={creditCard}/>
+                 className='purchase-detail__total'
+                 title='Total'
+                 value={total}/>
+                <PurchaseDetailLabel
+                 className='purchase-detail__credit-card'
+                 title='Credit Card'
+                 value={creditCard}/>
+                 <a className='purchase-detail__track-shipment'>Track Shipment</a>
+                 <a className='purchase-detail__print-receipt'>Print Receipt</a>
             </div>
         )
     }
